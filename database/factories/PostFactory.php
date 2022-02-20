46d85beb020e9c60e318f73a744f968d38b40538
Oleksii Derzhuk <oleksii.derzhuk@gmail.com>
2022-02-20 21:51:46 +0200

    working .htaccess -> public folder :)

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -3,6 +3,8 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Faker\Generator as Faker;
+use Stringable;
 
 class PostFactory extends Factory
 {
@@ -13,8 +15,18 @@ class PostFactory extends Factory
      */
     public function definition()
     {
+        $faker = new Faker;
+        $title = $faker->realText(rand(10, 40));
+        $short_title = mb_strlen($title)>30 ? mb_substr($title, 0, 30) . '...' : $title;
+        $cereated = $faker->fateTimeBetween('-30 days', '-1 days');
+
         return [
-            //
+            'title' => $title,
+            'short_title' => $short_title,
+            'author_id' => rand(1, 4),
+            'descr' => $faker->realText(rand(100, 500)),
+            'created_at' => $created,
+            'updated_at' => $created,
         ];
     }
 }
